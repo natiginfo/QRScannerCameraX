6e4c502ba68b3eb24c9acde1c684302198905648
Natig Babayev <natig.nizami@gmail.com>
2019-07-09 20:57:39 +0300

    initial project

---
+++ app/src/main/java/com/natigbabayev/qrscanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.natigbabayev.qrscanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
