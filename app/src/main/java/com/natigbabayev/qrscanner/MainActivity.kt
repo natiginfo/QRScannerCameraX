6e018d3b6a3e8be21496aba9b82f1024d1daf71a
Natig Babayev <natig.nizami@gmail.com>
2019-07-13 15:18:55 +0300

    Added dependencies and permission checking

--- app/src/main/java/com/natigbabayev/qrscanner/MainActivity.kt
+++ app/src/main/java/com/natigbabayev/qrscanner/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.natigbabayev.qrscanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.TextureView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 10
+    }
+
+    private lateinit var textureView: TextureView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textureView = findViewById(R.id.texture_view)
+
+        // Request camera permissions
+        if (isCameraPermissionGranted()) {
+            textureView.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
+    }
+
+    private fun startCamera() {
+        // We will implement this in next steps.
+    }
+
+    private fun isCameraPermissionGranted(): Boolean {
+        val selfPermission = ContextCompat.checkSelfPermission(baseContext, Manifest.permission.CAMERA)
+        return selfPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (isCameraPermissionGranted()) {
+                textureView.post { startCamera() }
+            } else {
+                Toast.makeText(this, "Camera permission is required.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
